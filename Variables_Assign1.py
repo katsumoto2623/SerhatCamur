be782fd00835805287950c1b21a24766f51edaa9
katsumoto2623 <69143811+katsumoto2623@users.noreply.github.com>
2020-08-03 16:08:16 +0300

    Add files via upload

---
+++ Variables_Assign1.py
@@ -0,0 +1,4 @@
+num1=1
+num2='1'
+print("The value and type of the first number are", num1,type(num1))
+print("The value and type of the second number are", num2,type(num2))
---
+++ Variables_Assign2.py
@@ -0,0 +1,5 @@
+num1=1
+num2='1'
+num3=int(num2)
+print("The value and type of the first number are", num1,type(num1))
+print("The value and type of the second number are", num3,type(num3))
